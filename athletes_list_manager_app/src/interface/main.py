493b0f073cf3dd781a3d142186469f7509dd451f
Frostnout <dima.burbas@gmail.com>
2026-04-19 19:39:12 +0300

    fix: spaces in main

--- athletes_list_manager_app/src/interface/main.py
+++ athletes_list_manager_app/src/interface/main.py
@@ -1,7 +1,8 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
 
-from main_window import Ui_MainWindow  
+from main_window import Ui_MainWindow
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -9,10 +10,11 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    
+
     window = MainWindow()
     window.show()
-    
+
     sys.exit(app.exec())
