9589c02d3bc7a4df20b476896f324b40653d3927
Frostnout <dima.burbas@gmail.com>
2026-04-21 14:15:02 +0300

    refactor: remove interface dir

---
+++ athletes_list_manager_app/src/main.py
@@ -0,0 +1,11 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+
+if __name__ == "__main__":
+    pass
