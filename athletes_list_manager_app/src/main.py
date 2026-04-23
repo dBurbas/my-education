15d243dfc194a37eabd73124d1c68ed2f67f1989
Frostnout <dima.burbas@gmail.com>
2026-04-23 09:17:37 +0300

    refactor: main
    Description:
    Make main to suit all previous changes in other files (controller, views, model, etc)

--- athletes_list_manager_app/src/main.py
+++ athletes_list_manager_app/src/main.py
@@ -2,24 +2,38 @@
 import sys
 
 from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QSettings
 
+from core.loader import SAXLoader
+from core.saver import DOMSaver
 from core.model import AthleteManagerModel
 from views.views import MainView
-from controllers import MainController
+from views.dialog_factory import DefaultDialogFactory
+
+from controllers import MainController, ThemeController, PaginationController
 
 
-def main():
-
+def main() -> None:
     app = QApplication(sys.argv)
 
-    model = AthleteManagerModel()
+    model = AthleteManagerModel(saver=DOMSaver(), loader=SAXLoader())
 
     view = MainView()
 
-    controller = MainController(model, view)
+    settings = QSettings("BSUIR", "SportMan")
+    theme_service = ThemeController(settings)
+    pagination = PaginationController(model, view)
+    dialog_factory = DefaultDialogFactory()
+
+    controller = MainController(
+        model=model,
+        view=view,
+        pagination=pagination,
+        theme_service=theme_service,
+        dialog_factory=dialog_factory,
+    )
 
     view.show()
-
     sys.exit(app.exec())
 
 
