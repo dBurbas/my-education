031fa8a5538111486226d66ff362d25ceae7b744
Frostnout <dima.burbas@gmail.com>
2026-04-23 09:04:08 +0300

    feat: implement saver
    Description:
    Implement saver interface and implement xml DOM saver

--- athletes_list_manager_app/src/core/saver.py
+++ athletes_list_manager_app/src/core/saver.py
@@ -1,9 +1,43 @@
 from abc import ABC, abstractmethod
+import xml.dom.minidom as minidom
+from core.athlete import Athlete
 
 
-# TODO: Typehints
 class ISaver(ABC):
-    # TODO: save method
     @abstractmethod
-    def save(self):
+    def save(self, athletes: list[Athlete], filepath: str):
         pass
+
+
+class DOMSaver(ISaver):
+    def __init__(self, encoding="utf-8"):
+        self.encoding = encoding
+
+    def save(self, athletes: list[Athlete], filepath: str):
+        document = minidom.Document()
+
+        root = document.createElement("athletes")
+        document.appendChild(root)
+
+        for athlete in athletes:
+            athlete_node = document.createElement("athlete")
+
+            def add_element(name, text_value):
+                node = document.createElement(name)
+                value = document.createTextNode(str(text_value))
+                node.appendChild(value)
+                athlete_node.appendChild(node)
+
+            add_element("fio", athlete.fio)
+            add_element("team", athlete.team)
+            add_element("position", athlete.position)
+            add_element("titles", athlete.titles)
+            add_element("sport", athlete.sport)
+            add_element("rank", athlete.rank)
+
+            root.appendChild(athlete_node)
+
+        with open(filepath, "w", encoding=self.encoding) as file:
+            document.writexml(
+                file, indent="  ", addindent="  ", newl="\n", encoding=self.encoding
+            )
