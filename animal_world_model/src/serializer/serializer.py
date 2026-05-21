70d84b783a7ab86b9d3a728abaf366edb61264ec
Frostnout <dima.burbas@gmail.com>
2026-05-21 12:03:13 +0300

    feat: implement saver and loader
    Description:
    Divide load and save methods, implement save and load logic of ecosystem in JSON.

--- animal_world_model/src/serializer/serializer.py
+++ animal_world_model/src/serializer/serializer.py
@@ -1,21 +1,72 @@
 from abc import ABC, abstractmethod
+import json
+from core.event_manager import EventManager
+from core.factory import OrganismFactory
+from core.ecosystem import Habitat, FoodChain, Ecosystem
+from exception.animal_world_exceptions import EcosystemLoadError
 
 
-class ISerializer(ABC):
+class ILoader(ABC):
     @abstractmethod
-    def save_to_file(self):
+    def load(self, filepath: str):
         pass
 
+
+class ISaver(ABC):
     @abstractmethod
-    def load_from_file(self):
+    def save(self, data, filepath: str) -> None:
         pass
 
 
-class JSONSerializer(ABC):
-    # TODO: save_to_file
-    def save_to_file(self):
-        pass
+class JSONLoader(ILoader):
+    def __init__(self, factory: OrganismFactory, encoding: str = "utf-8"):
+        self._encoding = encoding
+        self._factory = factory
 
-    # TODO: load_from_file
-    def load_from_file(self):
-        pass
+    def load(self, filepath: str):
+        with open(file=filepath, mode="r", encoding=self._encoding) as file:
+            data: dict = json.load(file)
+            if len(data) == 0 or not data["food_chain"] or not data["habitat"]:
+                raise EcosystemLoadError("No needed data in file")
+            habitat = Habitat.from_dict(data["habitat"])
+            diet_rules = {}
+            for eater_name, prey_names in data["food_chain"].items():
+                eater_cls = self._factory.species_to_type(eater_name)
+                prey_classes = [self._factory.species_to_type(p) for p in prey_names]
+                diet_rules[eater_cls] = prey_classes
+
+            food_chain = FoodChain(diet_rules=diet_rules)
+            organisms = []
+            for org_data in data["organisms"]:
+                species_name = org_data.pop("type")
+                name = org_data.pop("name")
+                x = org_data.pop("x")
+                y = org_data.pop("y")
+                saved_id = org_data.pop("organism_id")
+
+                org = self._factory.create_organism(
+                    species=species_name,
+                    name=name,
+                    x=x,
+                    y=y,
+                    organism_id=saved_id,
+                    **org_data,
+                )
+                organisms.append(org)
+            return Ecosystem(
+                event_manager=EventManager(),
+                habitat=habitat,
+                organisms=organisms,
+                food_chain=food_chain,
+                factory=self._factory,
+            )
+
+
+class JSONSaver(ISaver):
+    def __init__(self, encoding="utf-8"):
+        self._encoding = encoding
+
+    def save(self, data: dict, filepath: str) -> None:
+        with open(file=filepath, mode="w", encoding=self._encoding) as file:
+            # ?: Нужно ли использовать skipkeys
+            json.dump(data, file, ensure_ascii=False, indent=2)
